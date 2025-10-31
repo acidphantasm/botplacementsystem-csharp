62e42a0753efa0c40c61cffdb2b1c65eb251b234
acidphantasm <acidphantasm@acidphantasm.com>
2025-10-31 13:28:44 -0500

    Update to 2.0.3

--- Server/BotPlacementSystem.cs
+++ Server/BotPlacementSystem.cs
@@ -16,7 +16,7 @@ public record ModMetadata : AbstractModMetadata, IModWebMetadata
     public override string Name { get; init; } = "Acid's Bot Placement System";
     public override string Author { get; init; } = "acidphantasm";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.3");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.2");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
